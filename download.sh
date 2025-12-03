70ccb0edc9737a96f2b32ad4b05cbefbdbf6d140
tim901231 <b09901020@ntu.edu.tw>
2025-12-03 02:00:25 +0000

    run_script

---
+++ download.sh
@@ -0,0 +1,7 @@
+gdown https://drive.google.com/uc?id=1dVVTXb2nUFULJFPCqqyf99UqWIFtG8pg -O data.zip
+unzip data.zip -d ./DG-Mesh
+rm data.zip
+
+gdown https://drive.google.com/uc?id=1nhiJ2FR0srTuguS0UNDaOPtsZ_xQaE91 -O checkpoints.zip
+unzip checkpoints.zip -d ./DG-Mesh
+rm checkpoints.zip
---
+++ generate_video_with_custom_traj.sh
@@ -0,0 +1,7 @@
+# to get trajectory file, run:
+python DG-Mesh/dgmesh/render_trajectory.py --config DG-Mesh/dgmesh/configs/iphone/lamp.yaml \
+    --start_checkpoint DG-Mesh/outputs/lamp/data_v4-2025-12-02_04-25-35/
+
+# to generate video with custom trajectory:
+python DG-Mesh/dgmesh/render_custom_trajectory.py --config DG-Mesh/dgmesh/configs/iphone/lamp.yaml \
+    --start_checkpoint DG-Mesh/outputs/lamp/data_v4-2025-12-02_04-25-35/ --custom_dxyz_path DG-Mesh/outputs/lamp/rendering-traj-data-2025-12-02_22-49-40/deformed_xyz.npy
